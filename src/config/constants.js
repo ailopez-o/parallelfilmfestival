a5618c12797085b8686b48235100a4d10f182329
Aitor Lopez de Audícana <aitorlpzaudikana@gmail.com>
2026-04-22 17:04:35 +0200

    STEP1

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+/**
+ * Global application constants.
+ */
+
+// Fallback images for movie posters
+export const FALLBACK_IMAGE = 'https://placehold.co/300x450/1a1a1f/94a3b8?text=Cinema+Poster';
+export const TBD_POSTER = '/coming-soon.png';
+
+// Default application limits (will be overridden by DB values)
+export const DEFAULT_MAX_PROPOSALS = 3;
+export const DEFAULT_MAX_VOTES = 5;
+
+// API Timeouts
+export const TMDB_PROXY_TIMEOUT = 12000;
